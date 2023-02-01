a7dec32243b4e35ed56b324b25496af655831084
James <jamesballsemail@gmail.com>
2023-02-01 11:23:49 +0000

    Created Service Provider

---
+++ src/HealthNotificationServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace J84115\HealthNotification;
+ 
+use Illuminate\Support\ServiceProvider;
+ 
+class HealthNotificationServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        //
+    }
+ 
+    public function boot()
+    {
+        //
+    }
+}
